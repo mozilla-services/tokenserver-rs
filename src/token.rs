f786924913c252ea6e674684833a53fc7b94b88f
Amrita Gupta <amritagupta727790@gmail.com>
2020-07-07 22:27:40 +0530

    using this error

--- src/token.rs
+++ src/token.rs
@@ -1,9 +1,34 @@
+use jsonwebtoken::errors::ErrorKind;
 use jsonwebtoken::{
     decode, encode, Algorithm, DecodingKey, EncodingKey, Header, TokenData, Validation,
 };
-use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Serialize, Deserialize)]
+use serde::{Deserialize, Serialize};
+use std::convert::From;
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum TokenError {
+    #[error("Token is Invalid")]
+    InvalidToken,
+    #[error("Issuer is Invalid")]
+    InvalidIssuer,
+    #[error("some other error")]
+    Unknown,
+}
+
+// convert the error more appropriately.
+impl From<jsonwebtoken::errors::Error> for TokenError {
+    fn from(err: jsonwebtoken::errors::Error) -> Self {
+        match err.kind() {
+            ErrorKind::InvalidToken => TokenError::InvalidToken,
+            ErrorKind::InvalidIssuer => TokenError::InvalidIssuer,
+            _ => TokenError::Unknown,
+        }
+    }
+}
+
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
 pub struct Claims {
     pub sub: String,
     pub company: String,
@@ -11,7 +36,7 @@ pub struct Claims {
     pub exp: i64,
 }
 
-pub fn decode_token(token: &str) -> jsonwebtoken::errors::Result<TokenData<Claims>> {
+pub fn verify_jwt_token(token: &str) -> Result<TokenData<Claims>, TokenError> {
     let pub_key = include_bytes!("public_rsa_key.pem");
 
     decode::<Claims>(
@@ -19,9 +44,10 @@ pub fn decode_token(token: &str) -> jsonwebtoken::errors::Result<TokenData<Claim
         &DecodingKey::from_rsa_pem(pub_key).unwrap(),
         &Validation::new(Algorithm::RS256),
     )
+    .map_err(From::from)
 }
 
-pub fn generate_token(my_claims: &Claims) -> String {
+pub fn generate_token(my_claims: &Claims) -> Result<String, TokenError> {
     let privkey_pem = include_bytes!("private_rsa_key.pem");
 
     encode(
@@ -29,16 +55,14 @@ pub fn generate_token(my_claims: &Claims) -> String {
         &my_claims,
         &EncodingKey::from_rsa_pem(privkey_pem).unwrap(),
     )
-    .unwrap()
+    .map_err(From::from)
 }
 
 #[cfg(test)]
-
 mod tests {
 
     use super::*;
     use chrono::Utc;
-    use jsonwebtoken::errors::ErrorKind;
 
     #[test]
     fn test_token() {
@@ -51,15 +75,20 @@ mod tests {
             exp: now + THREE_DAYS,
         };
 
-        let token: String = generate_token(&my_claims);
-
-        let token_data = match decode_token(&token) {
-            Ok(c) => c,
-            Err(err) => match *err.kind() {
-                ErrorKind::InvalidToken => panic!("Token is invalid"), // Example on how to handle a specific error
-                ErrorKind::InvalidIssuer => panic!("Issuer is invalid"), // Example on how to handle a specific error
-                _ => panic!("Some other errors"),
-            },
+        let token: String = generate_token(&my_claims).unwrap();
+        println!("Original: {:?}", my_claims);
+        match verify_jwt_token(&token) {
+            Ok(value) => {
+                println!(" decoded: {:?}", value.claims);
+                assert!(value.claims == my_claims);
+            }
+            Err(err) => panic!("error: {}", err),
         };
+    }
+    #[test]
+    fn test_invalid_token() {
+        let token: String = String::from("bhxkgadweahfjhaweglfvawjcj");
+
+        assert!(verify_jwt_token(&token).is_err());
     }
 }
